76eb67cac80ecd24a69a62c7b7ce0109da3cb95d
msanchez <pch.sanch@gmail.com>
2019-07-11 15:58:59 +0200

    - add missing routes - change double quoutes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import routes from "./routes";
 
 // Create Express server
 const app = express();
@@ -10,7 +11,8 @@ app.set("port", process.env.PORT || 3001);
 
 app.use(bodyParser.json());
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
+app.use("/api", routes());
 
 app.get("/", (req, res) => {
     res.send("Spark Networks API");
