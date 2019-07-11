ddc2e6ec713d528336f3c941058a21d0b538eb8a
msanchez <pch.sanch@gmail.com>
2019-07-11 15:43:41 +0200

    initial setup react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+interface Props {}
+
+export default class FilterModule extends React.Component<Props> {
+
+render () {
+    return (
+    <div>
+      <h1> Welcome </h1>
+      </div>
+    );
+  }
+}
