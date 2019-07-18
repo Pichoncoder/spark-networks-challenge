92cfbff793bfe989b46da1d0b0e85efd70f382cc
Manuel Sanchez <pch.sanch@gmail.com>
2019-07-18 19:55:15 +0200

    add User Details  snapshot

---
+++ src/components/user-details/user-details.component.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import TestRenderer, { create } from 'react-test-renderer';
+import UserDetails from './user-details.component';
+import { shallow } from "enzyme";
+import { UserProps } from '../../types';
+
+describe('<UserDetails /> spec', () => {
+  it('Snapshot:check the component', () => {
+    const props: UserProps = { 
+        details: {
+            display_name: 'Chuck',
+            age: 33,
+            job_title: 'Ninja',
+            height_in_cm: 170,
+            city: 'US',
+            compatibility_score: 3,
+            contacts_exchanged: 0,
+            favourite: false,
+            religion: 'Jew',
+        } 
+    };
+
+      const container = TestRenderer.create(<UserDetails {...props}></UserDetails>);
+      expect(container).toMatchSnapshot();
+  });
+});
--- src/components/user-details/user-details.component.tsx
+++ src/components/user-details/user-details.component.tsx
@@ -4,32 +4,35 @@ import "./user-details.component.scss";
 
 const UserDetails: React.FC<UserProps> = (props: UserProps) => {
 
-return (
-<div className="user-details card">
-    <div className="user-details__pic">
-        <img className="user-details__pic-img" src={props.details.main_photo  ? props.details.main_photo  : "https://via.placeholder.com/100"}></img>
-    </div>
+    return (
+        <div className="user-details card">
+            <div className="user-details__pic">
+                <img className="user-details__pic-img"
+                    alt={props.details.display_name}
+                    src={props.details.main_photo ? props.details.main_photo : "https://via.placeholder.com/100"}>
+                </img>
+            </div>
 
-    <ul className="user-details__box --top-info">
-        <li className="user-details__li"><span className="user-details__name">{ props.details.display_name }</span></li>
-        <li className="user-details__li"><span className="user-details__job">{ props.details.job_title }</span></li>
-        <li className="user-details__li"><span className="user-details__fav">{ props.details.favourite }</span></li>
-    </ul>
+            <ul className="user-details__box --top-info">
+                <li className="user-details__li"><span className="user-details__name">{props.details.display_name}</span></li>
+                <li className="user-details__li"><span className="user-details__job">{props.details.job_title}</span></li>
+                <li className="user-details__li"><span className="user-details__fav">{props.details.favourite}</span></li>
+            </ul>
 
-    <ul className="user-details__box --buttom-info">
-        <li className="user-details__li --split-box">
-             <span className="user-details__religion">{ props.details.religion }</span>
-             <span className="user-details__city">{ props.details.city.name }</span>
-        </li>
-        <li className="user-details__li --split-box">
-            <span className="user-details__age">{ props.details.age }</span>
-            <br></br>
-            <span className="user-details__height">{ props.details.height_in_cm }</span>
-        </li>
-        <li className="user-details__li"><span className="user-details__contacts">{ props.details.contacts_exchanged }</span></li>
-        <li className="user-details__li"><span className="user-details__score">{ props.details.compatibility_score }</span></li>
-    </ul>
-</div>);
+            <ul className="user-details__box --buttom-info">
+                <li className="user-details__li --split-box">
+                    <span className="user-details__religion">{props.details.religion}</span>
+                    {/* <span className="user-details__city">{props.details.city.name}</span> */}
+                </li>
+                <li className="user-details__li --split-box">
+                    <span className="user-details__age">{props.details.age}</span>
+                    <br></br>
+                    <span className="user-details__height">{props.details.height_in_cm}</span>
+                </li>
+                <li className="user-details__li"><span className="user-details__contacts">{props.details.contacts_exchanged}</span></li>
+                <li className="user-details__li"><span className="user-details__score">{props.details.compatibility_score}</span></li>
+            </ul>
+        </div>);
 };
 
 export default UserDetails;
